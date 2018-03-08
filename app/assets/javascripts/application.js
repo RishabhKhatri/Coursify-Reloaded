70f858becc7aac3b112ac89ed303b833aa892b2e
Rishabh Khatri <rishabh15077@iiitd.ac.in>
2018-03-08 06:51:17 +0530

    Minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require ckeditor/init
+//= require turbolinks
 //= require_tree .
